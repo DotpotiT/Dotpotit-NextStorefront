c5a420ae3fe5ade930dc312063b4d64001eaf3b5
shakibul22 <shakibulislam684@gmail.com>
2024-02-22 17:58:59 +0600

    Footer added at layout page

--- src/app/(with-navbar)/layout.tsx
+++ src/app/(with-navbar)/layout.tsx
@@ -1,10 +1,12 @@
+import { Footer } from "@/Components/Footer/Footer";
 import Navbar from "@/Components/Navbar/Navbar";
 
 const WithNavbar = ({ children }) => {
   return (
     <div>
         <Navbar/>
-      <div className="w-full min-h-[70vh]">{children}</div>
+      <div className="w-full ">{children}</div>
+      <Footer/>
     </div>
   );
 };
