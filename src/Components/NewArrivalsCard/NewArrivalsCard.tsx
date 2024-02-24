9b775a97e7ffd873ed8ba9f1ed2eb2dd0f56bf8d
shakibul22 <shakibulislam684@gmail.com>
2024-02-24 16:18:25 +0600

    NEW ARRIVALS CARD NADDED

--- src/Components/NewArrivalsCard/NewArrivalsCard.tsx
+++ src/Components/NewArrivalsCard/NewArrivalsCard.tsx
@@ -20,29 +20,26 @@ const NewArrivalsCard: React.FC<NewArrivalsCardProps> = ({ product }) => {
 
   return (
     <div className="mb-2 sm:mb-0 relative overflow-hidden">
-      <div className="shadow-xl relative">
-        <div
-          className="transition-transform duration-500 transform hover:scale-110"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <Image
-            className={`w-full h-[350px] ${isHovered ? 'scale-110' : 'scale-100'}`}
-            src={image}
-            alt={name}
-            width={500}
-            height={400}
-          />
+      <div
+        className="shadow-xl transition-transform duration-500 transform hover:scale-110"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <Image
+          className={`w-full h-[350px] ${isHovered ? 'scale-110' : 'scale-100'}`}
+          src={image}
+          alt={name}
+          width={500}
+          height={400}
+        />
+        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <Link href={`/productDetails/${id}`} passHref>
+  <button className="flex justify-center items-center">
+    <AiOutlinePlus />
+  </button>
+</Link>
+
         </div>
-        {isHovered && (
-          <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50 transition-opacity duration-300">
-            <Link href={`/productDetails/${id}`} passHref>
-              <button className="flex justify-center items-center text-white">
-                <AiOutlinePlus className="text-5xl" />
-              </button>
-            </Link>
-          </div>
-        )}
       </div>
       <div className="product-description p-2 mt-4">
         <p className="">{price}</p>
