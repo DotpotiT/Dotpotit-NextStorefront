4cee6f10341cc3a9111bf2918d2016fbbdb86e24
shakibul22 <shakibulislam684@gmail.com>
2024-02-27 17:49:06 +0600

    footer error fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,15 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-    
-      domains: ['res.cloudinary.com','images.unsplash.com','source.unsplash.com','images.pexels.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'images.unsplash.com',
+      'source.unsplash.com',
+      'images.pexels.com',
+      // Add more hostnames here if needed
+    ],
+  },
+};
+
+export default nextConfig;
